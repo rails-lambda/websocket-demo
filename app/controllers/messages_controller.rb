26e3b9fd9b076d2446f61b84a645370a3bb84c21
Ken Collins <ken@metaskills.net>
2023-03-25 19:59:09 -0400

    Turbo Streams

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,10 @@ class MessagesController < ApplicationController
 
   def create
     @message = room.messages.create!(message_params)
-    redirect_to(room)
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to room }
+    end
   end
   
   private
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :room
   validates :content, presence: true
-
   scope :recent, -> { order(created_at: :desc).limit(20) }
+  
+  after_create_commit -> { broadcast_prepend_to room }
+  after_update_commit -> { broadcast_replace_to room }
+  after_destroy_commit -> { broadcast_remove_to room }
 end
