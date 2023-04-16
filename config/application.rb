156fa9d6235d82ea8885ff9a9454c331ecc60185
Ken Collins <ken@metaskills.net>
2023-04-16 09:41:25 -0400

    Move temp LambdaCable to lib dir.

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require_relative '../app/models/lambda_cable'
+require 'lambda_cable' if Rails.env.production?
 
 module LambyW
   class Application < Rails::Application
