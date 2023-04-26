f2a845f36277ff06075cddf5b39b812334a0192d
Ken Collins <ken@metaskills.net>
2023-04-26 00:36:15 +0000

    Try forced timeout.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import { Turbo, cable } from "@hotwired/turbo-rails"
 import "controllers"
+
+window.LambdaCable = {};
+LambdaCable.consumer = await cable.getConsumer();
+setInterval(() => {
+  if (Turbo.session.enabled) {
+    LambdaCable.consumer.connection.monitor.recordPing();
+  }
+}, 3000);
