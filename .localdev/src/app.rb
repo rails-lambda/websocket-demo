f858a58960442ee9df443a62dbc2f235f308fbd5
Ken Collins <ken@metaskills.net>
2023-06-01 19:25:42 -0400

    Final updates

--- .localdev/src/app.rb
+++ .localdev/src/app.rb
@@ -1,4 +1,3 @@
-v = 6
 ENV['BUNDLE_GEMFILE'] = '/var/task/Gemfile'
 require 'bundler/setup'
 require 'net/http'
@@ -9,7 +8,6 @@ require 'uri'
 def handler(event:, context:)
   context_data = context.instance_variables.each_with_object({}) { |ivar, memo| memo[ivar.to_s.sub('@','')] = context.instance_variable_get(ivar) }
   payload = { event: event, context: context_data }.to_json
-  puts "PAYLOAD: #{payload}"
   uri = URI.parse("http://#{ENV['TS_REMOTE_PROXY_HOST']}:#{ENV['TS_REMOTE_PROXY_PORT']}/")
   response_body = Net::HTTP.socks_proxy('localhost', 1055).start(uri.host, uri.port) do |http|
     request = Net::HTTP::Post.new(uri.request_uri, 
@@ -21,7 +19,12 @@ def handler(event:, context:)
     response = http.request(request)
     response.body
   end
-  response_json = JSON.parse(response_body)
-  puts "RESPONSE: #{response_json}"
+  response_json = begin
+    JSON.parse(response_body)
+  rescue
+    { statusCode: 500,
+      headers: { 'Content-Type' => 'text/html' },
+      body: '<h1>ProxyError</h1>' }
+  end
   response_json
 end
